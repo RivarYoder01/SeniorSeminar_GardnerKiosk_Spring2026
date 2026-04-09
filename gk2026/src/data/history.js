d1f5b58a3eaae49660db07c76c6df8cf2af7a7cd
RivarYoder01 <alyssa.stars.101@gmail.com>
2026-04-09 18:05:30 -0500

    Added new images to the history section and updated the index.css file to remove the translateY property from the slideInFromTop animation.

--- gk2026/src/data/history.js
+++ gk2026/src/data/history.js
@@ -3,10 +3,10 @@ export const history = [
     "gardnerNow": "https://www.wsc.edu/images/hero_document_gardner_hall.jpg",
     "historySummary": "Gardner Hall houses the Business and Economics Department at Wayne State. The facility connects to the Center for Applied Technology via a second-story catwalk, providing for all-weather transitions between the two buildings. Gardner Hall, a modern and spacious 40,000 square foot facility, was  built in 1994. Its 200-seat auditorium serves as a great location for  speakers and other events on campus.",
     "historyDocuments1": "/statementofneed.png",
-    "historyDocuments2": "https://amzirlodp-prd-s3.s3.amazonaws.com/documents/images/big_441d7f43ead31df601a2dc864f5d8ec564d2e7ae.jpg",
-    "historyDocuments3": "https://amzirlodp-prd-s3.s3.amazonaws.com/documents/images/big_441d7f43ead31df601a2dc864f5d8ec564d2e7ae.jpg",
-    "historyDocuments4": "https://amzirlodp-prd-s3.s3.amazonaws.com/documents/images/big_441d7f43ead31df601a2dc864f5d8ec564d2e7ae.jpg",
-    "historyDocuments5": "https://amzirlodp-prd-s3.s3.amazonaws.com/documents/images/big_441d7f43ead31df601a2dc864f5d8ec564d2e7ae.jpg",
-    "historyDocuments6": "https://amzirlodp-prd-s3.s3.amazonaws.com/documents/images/big_441d7f43ead31df601a2dc864f5d8ec564d2e7ae.jpg"
+    "historyDocuments2": "/schedule.png",
+    "historyDocuments3": "/sideview.png",
+    "historyDocuments4": "/sideview1.png",
+    "historyDocuments5": "/schematic.png",
+    "historyDocuments6": "/schematic1.png"
   }
 ]
